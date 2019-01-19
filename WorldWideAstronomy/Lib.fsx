9791a6064d1c24b481ba0ce34933715ce4445450
DEVP50\kalvis <kalvis@latnet.lv>
2019-01-19 13:26:19 +0200

    Mutable and ref parameter passing

--- WorldWideAstronomy/Lib.fsx
+++ WorldWideAstronomy/Lib.fsx
@@ -1,8 +1,14 @@
 #r @"C:\Users\kalvis\Documents\Projects\wwa\WorldWideAstronomy\WWA\bin\Debug\WWA.dll"
 
 open WorldWideAstronomy
+// Classic F# reference
 let m1 = ref 0.0
 let m2 = ref 0.0
 let e = WWA.wwaCal2jd(2018, 5, 12, m1, m2)
 printfn "%A %A " !m1 !m2
-//let rc = WWA
+
+// Use mutable as from F# 4.0, enable warnings to see stack or heap allocation
+let mutable e1 = 1.0
+let mutable e2 = 1.0
+WWA.wwaCal2jd(2018, 5, 12, &e1, &e2) |> ignore
+printfn "%A %A " e1 e2
