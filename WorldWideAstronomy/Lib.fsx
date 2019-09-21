2c17ec417a3d889a550615af28b66322f263d13e
DEVP50\kalvis <kalvis@latnet.lv>
2019-09-21 13:50:42 +0300

    Saveoint

--- WorldWideAstronomy/Lib.fsx
+++ WorldWideAstronomy/Lib.fsx
@@ -1,6 +1,8 @@
 #r @"C:\Users\kalvis\Documents\Projects\wwa\WorldWideAstronomy\WWA\bin\Debug\WWA.dll"
 
 open WorldWideAstronomy
+open System
+
 // Classic F# reference
 let m1 = ref 0.0
 let m2 = ref 0.0
@@ -12,3 +14,8 @@ let mutable e1 = 1.0
 let mutable e2 = 1.0
 WWA.wwaCal2jd(2018, 5, 12, &e1, &e2) |> ignore
 printfn "%A %A " e1 e2
+
+let pi_d = 3.14159265358979323846
+printfn "pi = %.33f\n   + %.33f\n   + %.33f\n" pi_d (sin(pi_d)) (Math.PI)
+let t = Math.Sin(1e22)
+printfn "%.33f" t
