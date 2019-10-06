7a5010ee1a1ba3b544368339073e3f673b9b9e36
DEVP50\kalvis <kalvis@latnet.lv>
2019-10-06 12:42:03 +0300

    SOFA GMST 1982 test and calling from F#

--- WorldWideAstronomy/Lib.fsx
+++ WorldWideAstronomy/Lib.fsx
@@ -1,5 +1,6 @@
-#r @"C:\Users\kalvis\Documents\Projects\wwa\WorldWideAstronomy\WWA\bin\Debug\WWA.dll"
-
+#I __SOURCE_DIRECTORY__
+#r @"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\netstandard.dll"
+#r @"C:\Users\kalvis\source\repos\wwa\WorldWideAstronomy\WWA.Core\bin\Debug\netcoreapp3.0\WWA.Core.dll"
 open WorldWideAstronomy
 open System
 
@@ -19,3 +20,14 @@ let pi_d = 3.14159265358979323846
 printfn "pi = %.33f\n   + %.33f\n   + %.33f\n" pi_d (sin(pi_d)) (Math.PI)
 let t = Math.Sin(1e22)
 printfn "%.33f" t
+
+WWA.wwaCal2jd(2017, 3, 17, &e1, &e2) |> ignore
+let gmst = WWA.wwaGmst82(e1, e2)
+
+let mutable sign = ' '
+let hms = Array.zeroCreate<int> 4
+//11h 38m 2.7608sec, Mica  2.7586 mine 1982: 2.7608
+let r = WWA.wwaA2tf(4, gmst, &sign, hms)
+
+let gmst2 = WWA.wwaGmst82(2457829.5, 0.0 )
+let r2 = WWA.wwaA2tf(6, gmst2, &sign, hms)
